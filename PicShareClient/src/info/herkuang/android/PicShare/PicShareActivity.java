cf730b5b6b5a8de7c38154f013add07c8ee0049e
Herkuang Miao <hikuimiao@gmail.com>
2012-02-23 20:20:00 +0800

    初始化工程

---
+++ PicShareClient/src/info/herkuang/android/PicShare/PicShareActivity.java
@@ -0,0 +1,13 @@
+package info.herkuang.android.PicShare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PicShareActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
