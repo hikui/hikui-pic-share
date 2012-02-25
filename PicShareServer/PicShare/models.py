56975f814de7d4021be549be295f2c87fd8f9343
Herkuang Miao <hikuimiao@gmail.com>
2012-02-25 13:42:01 +0800

    add some models. Other models are still under design.

--- PicShareServer/PicShare/models.py
+++ PicShareServer/PicShare/models.py
@@ -1,1 +1,27 @@
 #coding:utf-8
+from django.db import models
+from django.contrib.auth.models import User
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=10, unique=True)
+    
+class Status(models.Model):
+    timestamp = models.DateTimeField()
+    image = models.CharField(max_length=255)
+    text = models.CharField(max_length=140, null=True, blank=True)
+    location = models.CharField(max_length=20, null=True, blank=True)
+    STATUS_TYPE_CHOICES=(
+        (1,u'原创'),
+        (2,u'转发'),
+        (3,u'封禁'),
+        (4,u'被举报'),
+    )
+    status_type = models.IntegerField(choices=STATUS_TYPE_CHOICES,default=1)
+    tag = models.ForeignKey(Tag, null=True,blank=True)
+    user = models.ForeignKey(User)
+    
+class UserAddition(models.Model):
+    user = models.OneToOneField(User)
+    head = models.CharField(max_length=255,null=True, blank=True)
+    location = models.CharField(max_length=20, null=True, blank=True)
+    introduction = models.TextField()
+    
